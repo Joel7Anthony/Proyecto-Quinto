ccaf71852e2761d30bd07a3f16a2da084e4fe7f5
IsmaelAndra <jic.andrade@yavirac.edu.ec>
2023-11-12 22:55:46 -0500

    Cambios 12/11/2023

--- src/app/home/contact-page/contact-page.module.ts
+++ src/app/home/contact-page/contact-page.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ContactPageComponent } from './contact-page.component';
 
 
 
 @NgModule({
   declarations: [
-    
+    ContactPageComponent
   ],
   imports: [
     CommonModule
   ]
 })
-export class HomeModule { }
+export class ContactPageModule { }
