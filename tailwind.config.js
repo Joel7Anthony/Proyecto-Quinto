522b9c0777514f243541ceac842e0351293b84eb
Joel7Anthony <anto777molina777@gmail.com>
2023-11-28 23:40:49 -0500

    front

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,27 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   purge: {
     enabled: true,
-    content: ['./src/**/*.{html,ts}'],
+    content: [
+      '*.html',
+      './assets/js/main.js'
+    ]
   },
-  content: [],
   theme: {
-    extend: {},
+    extend: {
+      inset: {
+        '100': '100%',
+      },
+
+      padding: {
+        '120': '120px',
+      },
+
+      colors: {
+        'theme-color': '#361CC1',
+        'theme-color-2': '#FE7A7B'
+      }
+    },
   },
+  variants: {},
   plugins: [],
 }
